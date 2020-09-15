c20a03c819dded9d9ca4f60840d417cfc803b9d7
Han Jing <hanqijing@gmail.com>
2020-09-14 23:05:19 -0700

    enable collection reading

--- pipeline/to_mongo.py
+++ pipeline/to_mongo.py
@@ -1,16 +1,21 @@
 from pymongo import MongoClient
-
 # pprint library is used to make the output look more pretty
-
 from pprint import pprint
+import pprint
+from dotenv import load_dotenv
+load_dotenv()
 
+import os
+
+
+username = os.getenv("MONGO_USR")
+password = os.getenv("MONGO_PWD")
+mongo_url = os.getenv("MONGO_URL")
+print(username, password)
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient('ezjb0.gp7ea.mongodb.net', 27017)
-
-db=client.staging
-
-# Issue the serverStatus command and print the results
-
-serverStatusResult=db.command("serverStatus")
-
-pprint(serverStatusResult)
+MURL=f'mongodb+srv://{username}:{password}@{mongo_url}?retryWrites=true&w=majority'
+client = MongoClient(MURL)
+db=client.stg
+print(db.list_collection_names())
+posts = db.posts
+pprint.pprint(posts.find_one())
